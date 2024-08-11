791a2adae5ac4e3642a95971c474b2388c77a14f
joker45op <abhiguj13579@gmail.com>
2024-08-12 02:13:53 +0530

    lib complete

--- auth53-client/src/AuthClient.js
+++ auth53-client/src/AuthClient.js
@@ -1,4 +1,4 @@
-const axios = require("axios")
+const axios = require("axios");
 
 class AuthClient {
   constructor(baseURL) {
@@ -6,21 +6,55 @@ class AuthClient {
     this.token = null;
   }
 
+  async register(userName, email, password, role) {
+    try {
+      const res = await this.api.post("register",{userName, email, password, role})
+      this.token = res.data.accessToken;
+      return { token: this.token };
+    } catch (error) {}
+  }
 
-  async login(email, password){
-    try{
-        const res = await this.api.post("/login", {email,password})
-        this.token = res.data.accessToken
-        return (this.token)
-    }
-    catch(error){
-        console.log(error);
-        throw error
+  async login(email, password) {
+    try {
+      const res = await this.api.post("login", { email, password });
+      this.token = res.data.accessToken;
+      return { token: this.token };
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
   }
 
-  
-}
+  async logout() {
+    try {
+      await this.api.delete("logout", {
+        data: { token: this.token },
+      });
+      this.token = null;
+      console.log("logout");
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 
+  async validateToken() {
+    try {
+      const res = await this.api.post(
+        "validateToken",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        }
+      );
+      // console.log(res);
+    } catch (error) {
+      console.log(error.response.data.error);
+      // throw error
+    }
+  }
+}
 
 module.exports = AuthClient;
--- auth53-server/test.js
+++ auth53-server/test.js
@@ -1,14 +1,20 @@
 const AuthClient = require("../auth53-client/src/AuthClient");
 
-try {
-  const client = new AuthClient("http://localhost:3000/auth");
-  const token = client
-    .login("abhi531@gmail.com", "Create53pass")
-    .then((res) => {
-      console.log(res);
-    });
+async function test() {
+  try {
+    const client = new AuthClient("http://localhost:3000/auth"); 
+    const c = await client.register("abhi53", "ari53s4@gmail.com", "Create53pass", "user");
+    console.log(c);
+    const token = await client.login("abhi531@gmail.com", "Create53pass");
+    console.log("Logged in with token:", token);
+    // setTimeout(()=>{
+    client.validateToken();
+    client.logout();
 
-  console.log("Logged in with token:", token);
-} catch (error) {
-  console.error("Error loading module or during authentication:", error);
+    // },3000)
+  } catch (error) {
+    console.error("Error loading module or during authentication:", error);
+  }
 }
+
+test();
