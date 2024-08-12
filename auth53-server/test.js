971fffeb275906eb75d16e4c828483d382d12538
joker45op <abhiguj13579@gmail.com>
2024-08-12 23:29:16 +0530

    lib publishedon npm

--- auth53-server/test.js
+++ auth53-server/test.js
@@ -1,13 +1,13 @@
-const AuthClient = require("../auth53-client/src/AuthClient");
+// const AuthClient = require("../auth53-client/src/AuthClient");
+
+const AuthClient = require("auth53-client")
 
 async function test() {
   try {
     const client = new AuthClient("http://localhost:3000/auth"); 
     const c = await client.register("abhi53", "ari53s4@gmail.com", "Create53pass", "user");
-    console.log(c);
     const token = await client.login("abhi531@gmail.com", "Create53pass");
-    console.log("Logged in with token:", token);
-    // setTimeout(()=>{
+    
     client.validateToken();
     client.logout();
 
